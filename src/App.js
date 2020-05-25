784a20e505ef0492f1a28d21c5d4d029445fb9d4
Camilo Alzate <camilo.alzate@carvajal.com>
2020-05-25 02:21:44 -0500

    Instalacion, implementacion y uso de Material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Table from '@material-ui/core/Table';  
+import TableBody from '@material-ui/core/TableBody';  
+import TableCell from '@material-ui/core/TableCell';  
+import TableContainer from '@material-ui/core/TableContainer';  
+import TableHead from '@material-ui/core/TableHead';  
+import TableRow from '@material-ui/core/TableRow';  
+import Paper from '@material-ui/core/Paper';
 
 export default class OpportunitiesList extends React.Component {
 
@@ -29,7 +36,38 @@ export default class OpportunitiesList extends React.Component {
         </p>
       </header>
       <section>
-      { this.state.opportunities.map(opportunity => <ul><li>{opportunity.number}</li><li>{opportunity.title}</li><li>{opportunity.agency}</li><li>{opportunity.status}</li><li>{opportunity.posted}</li><li>{opportunity.close}</li></ul>)}
+        <TableContainer component={Paper}>  
+          <Table stickyHeader  aria-label="sticky table">  
+              <TableHead>  
+              <TableRow>  
+                  <TableCell>Id</TableCell>  
+                  <TableCell align="right">Number</TableCell>  
+                  <TableCell align="right">Title</TableCell>  
+                  <TableCell align="right">Agency</TableCell>  
+                  <TableCell align="right">Status</TableCell>  
+                  <TableCell align="right">Posted Date</TableCell>  
+                  <TableCell align="right">Closed Date</TableCell>  
+              </TableRow>  
+              </TableHead>  
+              <TableBody>  
+              {  
+                  this.state.opportunities.map((p, index) => {  
+                  return <TableRow key={index}>  
+                      <TableCell component="th" scope="row">  
+                      {p.id}  
+                      </TableCell>  
+                      <TableCell align="right">{p.number}</TableCell>  
+                      <TableCell align="right">{p.title}</TableCell>  
+                      <TableCell align="right">{p.agency}</TableCell>  
+                      <TableCell align="right">{p.status}</TableCell>  
+                      <TableCell align="right">{p.posted}</TableCell>  
+                      <TableCell align="right">{p.close}</TableCell>  
+                  </TableRow>  
+                  })  
+              }  
+              </TableBody>  
+          </Table>  
+        </TableContainer>
       </section>
     </div>  
     )
